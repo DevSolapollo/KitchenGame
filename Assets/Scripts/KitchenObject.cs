13abdb56305f177da1100c95354ef2fb64bec4c6
daysts232 <devsolapollo@gmail.com>
2025-04-03 14:16:15 +1300

    Kitchen Objects

---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public KitchenObjectSO GetKitchenObjectSO() {
+        return kitchenObjectSO;
+    }
+
+}
